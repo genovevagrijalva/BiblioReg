6b340433138ae8ee1e62b9e512211c4276647122
genovevagrijalva <bevitha-@hotmail.com>
2016-09-25 23:47:51 -0700

    Add files via upload

---
+++ BiblioReg/BiblioReg/frmLogin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BIBLIOREG_Lib;
+using System.IO;
+
+namespace BiblioReg
+{
+    public partial class frmLogin : Form
+    {
+       
+        public frmLogin()
+        {
+            InitializeComponent();
+            lblHora.Text = DateTime.Now.ToString();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblHora.Text = DateTime.Now.ToString();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if(txtUser.Text=="ADM" && txtPassword.Text=="123")
+            {
+                frmPrincipal frm = new frmPrincipal();
+                frm.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Usuario no encontrado");
+            }
+            txtUser.Text = "";
+            txtPassword.Text = "";
+            txtUser.Focus();
+        }
+
+        private void txtUser_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) txtPassword.Focus();
+        }
+
+        private void txtPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) btnAceptar.Focus();
+        }
+    }
+}
---
+++ BiblioReg/BiblioReg/frmPrincipal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BiblioReg
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aDMINISTRACIONToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmLibro libro = new frmLibro();
+            libro.ShowDialog();
+        }
+
+        
+
+        private void mANUALDEUSUARIOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmManual manual = new frmManual();
+            manual.ShowDialog();
+        }
+
+        private void aCERCADEToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmAcerca acerca = new frmAcerca();
+            acerca.ShowDialog();
+        }
+
+        
+        
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+    }
+}
